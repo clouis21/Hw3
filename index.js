d9cf44ec3c2757d905e3b1efdbe9afecb0e832ab
Chad <chadlouis21@yahoo.com>
2022-06-16 21:21:12 -0400

    second dry run

--- index.js
+++ index.js
@@ -1,32 +1,79 @@
-var a = document.getElementById('num1').value;
-var b = document.getElementById('num2').value;
+
+//Input value still takes default values. Need to update
+
+/*
+document.getElementById("myButton").onclick = function(){
+  var myValue = document.getElementById("myText").value;
+  console.log("This is the number", myValue)
+var num1 = parseInt(a);
+var num2 = parseInt(b);
+
+var num3 = parseInt(c);
+var num4 = parseInt(d);
+
+var a = prompt("Enter column first Value", "Insert Min Col");
+var b = prompt("Enter column second Value", "Insert Max Col");
+var c = prompt("Enter row first Value", "Insert Min Row");
+var d = prompt("Enterrow second Value", "Insert Max Row");
+*/
+
+const a = document.getElementById('num1').value;
+let b = document.getElementById('num2').value;
 var c = document.getElementById('num3').value;
 var d = document.getElementById('num4').value;
-//Input value still takes default values. Need to update
-var x = b - a;
-var y = d - c;
 
-function generate(num, fn) {
-  var array = Array(num), i = 0;
-  while (i < num) array[i] = fn(i++);
-  return array;
+var z = document.getElementById('num4');
+
+console.log(z);
+console.log(d);
+
+
+
+
+
+
+function multiplyTable(num1,num2,num3,num4) {
+
+  tbody = document.querySelector('tbody');
+  result = '';
+  row = tbody.insertRow();
+  row.insertCell().textContent = '';//Leave first column empty
+  for (let y = num3; y <= num4; y++) {
+    
+    if (y === c) {
+      for (let x = num1; x <= num2; x++) {
+        row.insertCell().textContent = x;
+      }
+    }
+    row = tbody.insertRow();
+  
+    row.insertCell().textContent = y;
+    for (let x = num1; x <= num2; x++) {
+      row.insertCell().textContent = x * y;
+    }
+  }
+  console.log('HELOOOOO');
+
 }
 
-var entry = (x, y) => `${x * y}`;
-var array2d = generate(x, row => generate(y, col => entry(row + 1, col + 1)));
-var result = array2d.map(row => row.join(' | ')).join('\n');
-
-console.log(result);
+console.log(num2);
+console.log(num3);
+console.log(num4);
 
 
-function multiplyBy()
-{
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2;
-};
 
-function genTable() {};
+multiplyTable(a,b,c,d);
+console.log(document.getElementById('num1').value);
+
+
+console.log(num1.value);
+
+
+
+
+
+
+
 //To Do 
 
 
