7489cdc5a7d88f1f7762032ccc41c01f67da8aee
Chad <chadlouis21@yahoo.com>
2022-06-20 16:01:26 -0400

    add comments

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 
 
 
-
+/* Functon is used to generate table by receiving four in puts from user.
+A starting row value a1 end value b1. Starting column value c1 end value d1.
+*/
 function multiplyTable_1(a1,b1,c1,d1) {
   var table1 = document.getElementsByTagName("TABLE")[0];
 
@@ -11,6 +13,7 @@ function multiplyTable_1(a1,b1,c1,d1) {
   console.log(descendants);
   descendants.remove();
 
+  // each value is innitialize in table
   console.log(table1);
   row = table1.insertRow(-1);
   row.insertCell(-1).textContent=a1;
@@ -18,23 +21,21 @@ function multiplyTable_1(a1,b1,c1,d1) {
   row = table1.insertRow(-1);
   row.insertCell(-1).textContent=c1;
   row.insertCell(-1).textContent=d1;
-  //divTabCont.append(table1.toString);
 }
 
     function multiplyTable(a1,b1,c1,d1) {
-      a1 = parseInt(a1);
+      a1 = parseInt(a1);//Parsint ensure input values are neterd as numerical integer
       b1 = parseInt(b1);
       c1 = parseInt(c1);
       d1 = parseInt(d1);
-
+      //Establishes table first position value. Fucus return cusor to opropriate field to be inputed
       var table1 = document.getElementsByTagName("TABLE")[0];
      var para1 = document.getElementById("para");
-    // para1.innerHTML = "";
-    if(a1 > b1){
+    if(a1 > b1){ //Ensures end valus is not less than starting value for colums
       document.getElementById('para1').innerHTML = "First Column Number Greater Than Second";
       document.getElementById("a").focus();
       return;
-    }
+    }//Ensures values is not greater than or less than the table limit |50|
       if(a1 < -50 || a1 > 50){ 
         document.getElementById('para1').innerHTML = "First Column Number Out of Bound";
         document.getElementById("a").focus();
@@ -45,7 +46,7 @@ function multiplyTable_1(a1,b1,c1,d1) {
         document.getElementById("b").focus();
         return;
       }
-      if(c1 > d1){
+      if(c1 > d1){//Ensures end valus is not less than starting value for rows
         document.getElementById('para1').innerHTML = "First Column Number Greater Than Second";
         document.getElementById("c").focus();
         return;
@@ -60,6 +61,8 @@ function multiplyTable_1(a1,b1,c1,d1) {
         document.getElementById("d").focus();
         return;
       }
+      //Prior to append descendants.remove() remove prior values from table
+
       descendants = document.getElementsByTagName('tbody')[0];
       console.log(descendants);
       descendants.remove();
@@ -68,7 +71,9 @@ function multiplyTable_1(a1,b1,c1,d1) {
       console.log(d1);
       console.log(table1);
      
-
+      /*Loops check for condition of table bound for starting columns based
+      on inputed values then multiply the coresponding rows and columns then append to 
+      table each value */
       console.log(table1);
       row = table1.insertRow();
       for (let y = c1; y <= d1; y++) {
@@ -88,6 +93,7 @@ function multiplyTable_1(a1,b1,c1,d1) {
       console.log(table1);
 
     }
+    multiplyTable(1,10,1,10)
 
 
       
